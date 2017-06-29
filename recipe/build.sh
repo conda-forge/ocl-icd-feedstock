5b991dcda49cde03f4a152e4fa8a77281d660450
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-29 15:27:55 -0400

    Make the custom ocl-icd vendor directory

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
 set -e
 
+export OCL_ICD_VENDORDIR="${PREFIX}/etc/OpenCL/vendors"
+
+mkdir -p "${OCL_ICD_VENDORDIR}"
+
 ./bootstrap
 
 ./configure \
   --prefix=$PREFIX \
   --enable-official-khronos-headers \
-  --enable-custom-vendordir=$PREFIX/etc/OpenCL/vendors
+  --enable-custom-vendordir="${OCL_ICD_VENDORDIR}"
 
 make
 make check
