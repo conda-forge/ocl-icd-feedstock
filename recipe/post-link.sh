c3e405a49843e2983a83e7b06fe9121c9f87506d
Andreas Kloeckner <inform@tiker.net>
2019-01-21 23:01:48 -0600

    Do not automatically link system ICDs

--- recipe/post-link.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ${PREFIX}/etc/OpenCL/vendors
-for filename in /etc/OpenCL/vendors/*.icd; do
-    ln -s ${filename} ${PREFIX}/etc/OpenCL/vendors/system-$(basename $filename) || true
-    echo "Symlinked $filename from system to conda opencl directory.">> "${PREFIX}/.messages.txt" 2>&1
-done
-
