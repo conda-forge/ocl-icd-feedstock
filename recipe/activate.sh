da3776641c77c897b88e9eabda34b9536c3e4cd0
Isuru Fernando <isuruf@gmail.com>
2019-10-27 10:40:56 -0500

    Update instructions

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,13 +1,13 @@
 conda_ocl_icd_activate () {
     if [[ -z "`ls ${CONDA_PREFIX}/etc/OpenCL/vendors/`" ]]; then
         echo "WARNING: No ICDs were found. Either,";
-        echo "- Install a conda package providing a OpenCL implementation (pocl, oclgrind) or ";
+        echo "- Install a conda package providing a OpenCL implementation (pocl, oclgrind, intel-compute-runtime, beignet) or ";
         if [[ ! -f /etc/OpenCL/vendors/*.icd ]]; then
             echo -n "- Make ";
         else
-            echo -n "- Install an OpenCL implementation on system-wide and make ";
+            echo -n "- Install an OpenCL implementation system-wide and make ";
         fi
-        echo "your system-wide implementation visible by doing 'ln -s /etc/OpenCL/vendors "${CONDA_PREFIX}"/etc/OpenCL/vendors'  ";
+        echo "your system-wide implementation visible by installing ocl-icd-system conda package. ";
     fi;
 }
 
